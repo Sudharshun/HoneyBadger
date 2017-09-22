0ac4538061b74d61cd4b90f8e6384ccfd953c8b8
Sudharshun Ravichander <Sudharshun@gmail.com>
2017-09-22 00:28:15 -0500

    Initial Commit

---
+++ recipes.js
@@ -0,0 +1,21 @@
+/* eslint-disable  func-names */
+/* eslint-disable max-len */
+/* eslint quote-props: ['error', 'consistent']*/
+module.exports = {
+    'ANSWERS_EN_GB': {
+        'intel': 'A cool project from a cool team that will change the way we do business for our customers',
+        'animal': 'A cool guy from the team that makes everything and breaks everything',
+        'retroguy': 'A Fall guy who volunteers to take notes during our Retro',
+        'honey badger': 'Our Team mascot, Honey badger dont care',
+        'auto guy': 'watch out, he might be here any moment and he has a million questions',
+        'platform': 'The Platform represents a revolutionary new way to engage our Customers. Become their Advisors and participate in their Journey. To signup , call your education team',
+    },
+    'ANSWERS_EN_US': {
+      'intel': 'A cool project from a cool team that will change the way we do business for our customers',
+      'animal': 'A cool guy from the team that makes everything and breaks everything',
+      'retroguy': 'A Fall guy who volunteers to take notes during our Retro',
+      'honey badger': 'Our Team mascot, Honey badger dont care',
+      'auto guy': 'watch out, he might be here any moment and he has a million questions',
+      'platform': 'The Platform represents a revolutionary new way to engage our Customers. Become their Advisors and participate in their Journey. To signup , call your education team',
+    },
+};
